18ac74c64aca64ab0907abd7c9c075294c9ae905
SANTU DE <62021656+santude1997@users.noreply.github.com>
2021-09-03 10:56:44 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path:'login',component:LoginComponent},
+  {path:'forgot-password',component:ForgotPasswordComponent},
+  {path:'admin',
+  canActivate:[AuthGuard],
+  loadChildren:()=>import('./modules/admin/admin.module').then((m) =>m.AdminModule)},
+  {path:'**',component:PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
